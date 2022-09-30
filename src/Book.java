fbfd3a7018d69567d743fb8df10e5a81bc07ed01
Alex <gimberg_aleksandr@mail.ru>
2022-09-30 18:55:58 +0700

    HomeWork ObjectsClasses

---
+++ src/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    private String tittle;
+    private int yearPublishing;
+    private Author author;
+
+
+    public Book(String tittle, Author author, int yearPublishing) {
+        this.tittle = tittle;
+        this.yearPublishing = yearPublishing;
+        this.author = author;
+    }
+
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public int getYearPublishing() {
+        return yearPublishing;
+    }
+
+    public void setYearPublishing(int yearPublishing) {
+        this.yearPublishing = yearPublishing;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    void BookSeal() {
+        System.out.println();
+        System.out.println(tittle + ", автор " + getAuthor().getFirstName() + " " + getAuthor().getLastName() + ", год " + yearPublishing);
+        System.out.println("---------------------------------------------------------------------------------------");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void task(String number) {
+        System.out.println("==============================================");
+        System.out.println("Задание " + number);
     }
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        task("1");
+
+        Book TheGreenMile = new Book("Зеленная миля", new Author("Stephen", "King"), 1996);
+
+        Book TheLordOfTheRings = new Book("Властелин колец", new Author("John", "Tolkien"), 1954);
+
+        TheGreenMile.BookSeal();
+        TheLordOfTheRings.BookSeal();
+
+        TheLordOfTheRings.setYearPublishing(1955);
+        TheLordOfTheRings.BookSeal();
+
+        }
 }
